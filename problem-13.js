0e8de382367646236ef098bcdc861bd290a25362
SabujMahonto <64306095+SabujMahonto@users.noreply.github.com>
2022-02-04 18:29:41 +0600

    feet to inch convater cooked

--- problem-13.js
+++ problem-13.js
@@ -1,12 +1,12 @@
 // ১. সিম্পল একটা ফাংশন লিখতে হবে। যেটার নাম হবে feetToInch এবং এই ফাংশন ইনপুট হিসেবে নিবে feet আর রিটার্ন করবে inch । অর্থাৎ এই ফাংশনকে কোন একটা ফিট বলে দিলে সে রিটার্ন হিসেবে বলে দিবে কত ইঞ্চি হয়।
 // 1. Simple is to write a function. Which will be called feetToInch and this function will take feet as input and return inch. That is, if you call this function a fit, it will tell you how many inches it is as a return.
 
-function feetToInch(inch){
+function feetToInch(feet){
 
-    let feet = inch / 12;
-    return feet
+    let inch = feet * 12;
+    return inch
 }
  
-let totalInch = feetToInch(60);
+let totalInch = feetToInch(12);
 console.log(totalInch);
 
